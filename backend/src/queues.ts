b37ced3f0714f1424e80a8135dcbe34bd61a2895
GeovannyMunoz <geo_roskax100pre@hotmail.com>
2025-07-11 09:11:05 -0500

    fix space end campaign

--- backend/src/queues.ts
+++ backend/src/queues.ts
@@ -687,12 +687,12 @@ async function handlePrepareContact(contactId, campaignId, delay, variables, cam
     campaignShipping.campaignId = campaignId;
 
     const message = getProcessedMessage(campaign.originalMessage, variables, contact);
-    campaignShipping.message = `${message}\u200c`;
+    campaignShipping.message = `${message} \u200c`;
     
 
     if (campaign.confirmation) {
       const message = getProcessedMessage(campaign.originalConfirmationMessages, variables,contact);
-      campaignShipping.confirmationMessage = `${message}\u200c`;
+      campaignShipping.confirmationMessage = `${message} \u200c`;
     }
 
     const [record, created] = await CampaignShipping.findOrCreate({
